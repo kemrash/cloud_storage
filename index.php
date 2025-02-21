b002ccd8ee370f2f79305a84a927d0cddfa707eb
Максим Шеметов <kemnep@is74.ru>
2025-02-21 11:58:56 +0500

    Добавление favicon, обновление ссылок на CSS и JS в index.html, улучшение правил перенаправления в .htaccess и удаление закомментированных заголовков CORS из index.php.

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ use Core\Router;
 
 require_once './autoload.php';
 
-// header('Access-Control-Allow-Origin: http://localhost:5173');
-// header('Access-Control-Allow-Credentials: true');
-// header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-// header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
 set_exception_handler([Helper::class, 'exceptionHandler']);
 
 $urlList = [

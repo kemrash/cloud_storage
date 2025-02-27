53b45a1ba6d7e8189fe3d37cc365e3838eec5d79
Максим Шеметов <kemnep@is74.ru>
2025-02-27 21:21:29 +0500

    Добавление методов для создания и управления папками, улучшение обработки ошибок и валидации данных при создании пользователя. Обновление класса Folder с новыми методами для работы с папками.

--- Controllers/FolderController.php
+++ Controllers/FolderController.php
@@ -6,13 +6,13 @@ use Core\App;
 use Core\Helper;
 use Core\Request;
 use Core\Response;
-use traits\PageTrait;
+use traits\StatusResponseTrait;
 use traits\UserTrait;
 
 class FolderController
 {
     use UserTrait;
-    use PageTrait;
+    use StatusResponseTrait;
 
     /**
      * Возвращает список папок пользователя.
@@ -21,11 +21,13 @@ class FolderController
      */
     public function list(): Response
     {
-        if ($response = $this->checkUserAuthorization()) {
-            return $response;
+        if (!$this->isLogin()) {
+            return $this->accessForbidden();
         }
 
-        return App::getService('folderService')->getUserFoldersList((int) $_SESSION['id']);
+        $data = App::getService('folder')->list((int) $_SESSION['id']);
+
+        return new Response('json', $data);
     }
 
     /**
@@ -36,8 +38,8 @@ class FolderController
      */
     public function add(Request $request): Response
     {
-        if ($response = $this->checkUserAuthorization()) {
-            return $response;
+        if (!$this->isLogin()) {
+            return $this->accessForbidden();
         }
 
         $validated = $this->getValidatedData($request, 'POST', 'parentId');
@@ -48,19 +50,36 @@ class FolderController
 
         [$userId, $parentId, $name] = $validated;
 
-        return App::getService('folderService')->createUserFolder($userId, $parentId, $name);
+        if ($parentId === 0) {
+            return new Response('json', Helper::showError('Нельзя создать еще одну корневую папку'), 400);
+        }
+
+        $folder = App::getService('folder');
+
+        if (!$folder->create($userId, $parentId, $name)) {
+            return new Response('json', Helper::showError('Папка с такими параметрами уже существует'), 400);
+        }
+
+        return new Response();
     }
 
     /**
      * Переименовывает папку пользователя.
      *
      * @param Request $request HTTP-запрос, содержащий данные для переименования папки.
-     * @return Response HTTP-ответ с результатом операции.
+     * @return Response HTTP-ответ, указывающий на результат операции.
+     *
+     * Метод проверяет, авторизован ли пользователь, валидирует входные данные и переименовывает папку.
+     * Если пользователь не авторизован, возвращается ответ с ошибкой доступа.
+     * Если входные данные не прошли валидацию, возвращается ответ с ошибкой валидации.
+     * Если папка не найдена, возвращается ответ с ошибкой "страница не найдена".
+     * Если папка с таким именем уже существует, возвращается ответ с ошибкой.
+     * В случае успешного переименования возвращается status ok.
      */
     public function rename(Request $request): Response
     {
-        if ($response = $this->checkUserAuthorization()) {
-            return $response;
+        if (!$this->isLogin()) {
+            return $this->accessForbidden();
         }
 
         $validated = $this->getValidatedData($request, 'PUT', 'id');
@@ -71,29 +90,58 @@ class FolderController
 
         [$userId, $id, $name] = $validated;
 
-        return App::getService('folderService')->renameUserFolder($userId, $id, $name);
+        $folder = App::getService('folder');
+
+        if (!$folder->get($userId, $id)) {
+            return $this->pageNotFound();
+        }
+
+        if (!$folder->rename($name)) {
+            return new Response('json', Helper::showError('Папка с таким названием уже существует в родительской папке'), 400);
+        }
+
+        return new Response();
     }
 
     /**
-     * Получает информацию о папке пользователя.
+     * Получает информацию о папке и её содержимом.
      *
-     * @param array<string> $params Массив параметров, где первый элемент - идентификатор папки.
-     * @return Response Ответ с информацией о папке или сообщение об ошибке.
+     * @param array<int, string> $params Массив параметров, где первый элемент - ID папки.
+     * @return Response Ответ в формате JSON с информацией о папке и её содержимом.
      */
     public function get(array $params): Response
     {
-        if ($response = $this->checkUserAuthorization()) {
-            return $response;
+        if (!$this->isLogin()) {
+            return $this->accessForbidden();
         }
 
-        if ($response = $this->checkNotFoundPage($params[0])) {
-            return $response;
+        if (!isset($params[0]) || !ctype_digit($params[0])) {
+            return $this->pageNotFound();
         }
 
         $userId = (int) $_SESSION['id'];
         $folderId = (int) $params[0];
 
-        return App::getService('folderService')->getUserFolder($userId, $folderId);
+        $folder = App::getService('folder');
+
+        if (!$folder->get($userId, $folderId)) {
+            return $this->pageNotFound();
+        }
+
+        $files = App::getService('file')->getFilesInFolder($folderId);
+
+        $result = [
+            'id' => $folderId,
+            'userId' => $userId,
+            'parentId' => $folder->parentId,
+            'name' => $folder->name,
+            'content' => [
+                'folders' => $folder->getFoldersInFolder(),
+                'files' => $files,
+            ],
+        ];
+
+        return new Response('json', $result);
     }
 
     /**
@@ -104,18 +152,30 @@ class FolderController
      */
     public function remove(array $params): Response
     {
-        if ($response = $this->checkUserAuthorization()) {
-            return $response;
+        if (!$this->isLogin()) {
+            return $this->accessForbidden();
         }
 
-        if ($response = $this->checkNotFoundPage($params[0])) {
-            return $response;
+        if (!isset($params[0]) || !ctype_digit($params[0])) {
+            return $this->pageNotFound();
         }
 
         $userId = (int) $_SESSION['id'];
         $folderId = (int) $params[0];
 
-        return App::getService('folderService')->removeUserFolder($userId, $folderId);
+        $folder = App::getService('folder');
+
+        if (!$folder->get($userId, $folderId)) {
+            return $this->pageNotFound();
+        }
+
+        if ($folder->parentId === 0) {
+            return new Response('json', Helper::showError('Нельзя удалить корневую папку'), 400);
+        }
+
+        $folder->delete();
+
+        return new Response();
     }
 
     /**
--- Models/File.php
+++ Models/File.php
@@ -2,8 +2,20 @@
 
 namespace Models;
 
+use Core\App;
+use Core\Config;
+use Core\Db;
+use Exception;
+use Flow\Basic;
+use Flow\Request;
+use Flow\Config as FlowConfig;
+use Flow\Uploader;
+use PDOException;
+use Ramsey\Uuid\Uuid;
+
 class File
 {
+    private const DB_NAME = 'file';
     private ?int $id;
     private int $userId;
     private int $folderId;
@@ -11,28 +23,6 @@ class File
     private string $origenName;
     private string $mimeType;
     private int $size;
-
-    /**
-     * Конструктор класса File.
-     *
-     * @param int $userId Идентификатор пользователя.
-     * @param int $folderId Идентификатор папки.
-     * @param string $serverName Имя файла на сервере.
-     * @param string $origenName Оригинальное имя файла.
-     * @param string $mimeType MIME-тип файла.
-     * @param int $size Размер файла в байтах.
-     * @param int|null $id Идентификатор файла (необязательный параметр).
-     */
-    public function __construct(int $userId, int $folderId, string $serverName, string $origenName, string $mimeType, int $size, ?int $id = null)
-    {
-        $this->id = $id;
-        $this->userId = $userId;
-        $this->folderId = $folderId;
-        $this->serverName = $serverName;
-        $this->origenName = $origenName;
-        $this->mimeType = $mimeType;
-        $this->size = $size;
-    }
 
     /**
      * Магический метод для получения значения свойства объекта.
@@ -44,6 +34,233 @@ class File
     {
         if (isset($this->$name)) {
             return $this->$name;
+        }
+    }
+
+    /**
+     * Возвращает список файлов для указанного пользователя.
+     *
+     * @param int $userId Идентификатор пользователя.
+     * @return array<int, array<string, mixed>> Массив файлов, где каждый файл представлен в виде ассоциативного массива с ключами:
+     * - 'id' (int): Идентификатор файла.
+     * - 'folderId' (int): Идентификатор папки.
+     * - 'serverName' (string): Имя файла на сервере.
+     * - 'origenName' (string): Оригинальное имя файла.
+     * - 'mimeType' (string): MIME-тип файла.
+     * - 'size' (int): Размер файла в байтах.
+     */
+    public function list(int $userId): array
+    {
+        $data = Db::findBy(
+            self::DB_NAME,
+            ['id', 'folderId', 'serverName', 'origenName', 'mimeType', 'size'],
+            Config::getConfig('database.dbColumns.file'),
+            ['userId' => $userId]
+        );
+
+        return $data === null ? [] : $data;
+    }
+
+    /**
+     * Получает данные файла из базы данных по заданным параметрам.
+     *
+     * @param array<string, mixed> $params Ассоциативный массив параметров для поиска файла.
+     * 
+     * @return bool Возвращает true, если файл найден и данные успешно загружены, иначе false.
+     */
+    public function get(array $params): bool
+    {
+        $data = Db::findOneBy(self::DB_NAME, $params, Config::getConfig('database.dbColumns.file'));
+
+        if ($data === null) {
+            return false;
+        }
+
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+
+        return true;
+    }
+
+    /**
+     * Получает файлы в указанной папке.
+     *
+     * @param int $folderId Идентификатор папки.
+     * @return array<int, array<string, mixed>> Массив файлов, где каждый файл представлен в виде ассоциативного массива с ключами 'id' и 'origenName'.
+     */
+    public function getFilesInFolder(int $folderId): array
+    {
+        return Db::findBy(self::DB_NAME, ['id', 'origenName', 'serverName'], Config::getConfig('database.dbColumns.file'), ['folderId' => $folderId]);
+    }
+
+    /**
+     * Добавляет чанки файла для указанного пользователя и папки.
+     *
+     * @param int $userId Идентификатор пользователя.
+     * @param int $folderId Идентификатор папки.
+     * @param Request $request Объект запроса, содержащий данные о загружаемом файле.
+     * @return array{
+     *     status: string,
+     *     message: string,
+     *     path?: string
+     * } Массив с результатом выполнения операции. В случае успешной загрузки файла возвращается путь к файлу.
+     * @throws Exception В случае возникновения ошибок при создании директорий, сохранении файла или генерации уникального имени файла.
+     */
+    public function addFileChunks(int $userId, int $folderId, Request $request): array
+    {
+        $config = new FlowConfig();
+        $chunksTempFolder = './chunks_temp_folder';
+        $uploadFolder = Config::getConfig('app.uploadFile.folderFileStorage');
+
+        try {
+            if (!is_dir($chunksTempFolder) && !mkdir($chunksTempFolder, 0755, true)) {
+                throw new Exception("Не удалось создать временную директорию: {$chunksTempFolder}");
+            }
+
+            if (!is_dir($uploadFolder) && !mkdir($uploadFolder, 0755, true)) {
+                throw new Exception("Не удалось создать директорию для хранения файлов: {$uploadFolder}");
+            }
+
+            $config->setTempDir($chunksTempFolder);
+            $uploadFileName = uniqid('', true) . "_" . Uuid::uuid4()->toString();
+            $uploadPath = $uploadFolder . $uploadFileName;
+
+            if (Basic::save($uploadPath, $config, $request)) {
+                $maxAttempts = 2;
+                $fileSize = filesize($uploadPath);
+                $fileMimeType = mime_content_type($uploadPath);
+
+                if ($fileMimeType === false) {
+                    $fileMimeType = '';
+                }
+
+                $this->userId = $userId;
+                $this->folderId = $folderId;
+                $this->serverName = $uploadFileName;
+                $this->origenName = $request->getFileName();
+                $this->mimeType = $fileMimeType;
+                $this->size = (int) $fileSize;
+
+                for ($i = 0; $i <= $maxAttempts; $i++) {
+                    try {
+                        $this->addFile();
+                        break;
+                    } catch (PDOException $e) {
+                        if ($e->getCode() !== '23000') {
+                            throw new Exception($e->getMessage());
+                        }
+                    }
+
+                    $oldFileName = $this->serverName;
+                    $this->serverName = uniqid('', true) . "_" . Uuid::uuid4()->toString();
+
+                    if ($i === $maxAttempts) {
+                        throw new Exception("Не удалось сгенерировать уникальное имя файла после {$maxAttempts} попыток.");
+                    }
+
+                    try {
+                        rename($uploadFolder . $oldFileName, $uploadFolder . $this->serverName);
+                    } catch (Exception $e) {
+                        throw new Exception($e->getMessage());
+                    }
+                }
+
+                $this->randomClearFolderChunks($chunksTempFolder);
+
+                return [
+                    'status' => 'success',
+                    'message' => 'Файл успешно загружен',
+                    'path' => $uploadPath
+                ];
+            }
+
+            return [
+                'status' => 'continue',
+                'message' => 'Чанк получен, продолжается загрузка'
+            ];
+        } catch (Exception $e) {
+            if (isset($this->serverName) && file_exists($uploadFolder . $this->serverName)) {
+                try {
+                    unlink($uploadFolder . $this->serverName);
+                } catch (Exception $e) {
+                    throw new Exception(__CLASS__ . ': ' . $e->getMessage());
+                }
+            }
+
+            throw new Exception(__CLASS__ . ': ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Добавляет файл в базу данных.
+     *
+     * Метод собирает информацию о файле в массив и вставляет его в базу данных.
+     * После успешной вставки, идентификатор нового файла сохраняется в свойство $this->id.
+     *
+     * @return void
+     */
+    public function addFile(): void
+    {
+        $addFile = [
+            'userId' => $this->userId,
+            'folderId' => $this->folderId,
+            'serverName' => $this->serverName,
+            'origenName' => $this->origenName,
+            'mimeType' => $this->mimeType,
+            'size' => $this->size,
+        ];
+
+        $this->id = Db::insert(self::DB_NAME, $addFile, Config::getConfig('database.dbColumns.file'));
+    }
+
+    /**
+     * Переименовывает файл.
+     *
+     * @param string $newName Новое имя файла.
+     * @return bool Возвращает true, если переименование прошло успешно, иначе false.
+     */
+    public function rename(string $newName): bool
+    {
+        $data = Db::updateOneBy(self::DB_NAME, ['origenName' => $newName], ['id' => $this->id], Config::getConfig('database.dbColumns.file'));
+
+        if (!isset($data) || isset($data['code']) && $data['code'] === '23000') {
+            return false;
+        }
+
+        $this->origenName = $newName;
+
+        return true;
+    }
+
+    /**
+     * Удаляет файл из базы данных и файлового хранилища.
+     *
+     * Метод удаляет запись о файле из базы данных, используя идентификатор файла,
+     * и затем удаляет сам файл из файлового хранилища.
+     *
+     * @return void
+     */
+    public function delete(): void
+    {
+        Db::deleteOneBy(self::DB_NAME, ['id' => $this->id], Config::getConfig('database.dbColumns.user'));
+        App::getService('fileStorage')->deleteFile($this->serverName);
+    }
+
+    /**
+     * Удаляет случайные части файлов в указанной папке.
+     *
+     * Этот метод с вероятностью 1% вызывает метод pruneChunks класса Uploader,
+     * который очищает части файлов в указанной папке.
+     *
+     * @param string $folder Путь к папке, в которой необходимо удалить части файлов.
+     *
+     * @return void
+     */
+    private function randomClearFolderChunks(string $folder): void
+    {
+        if (1 == mt_rand(1, 100)) {
+            Uploader::pruneChunks($folder);
         }
     }
 }
